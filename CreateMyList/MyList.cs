c2614b10000ea7905dcd64022a4bd318c3af555c
Nazım Marul <nazmmarul@hotmail.com>
2021-01-21 15:48:32 +0300

    Add project files.

---
+++ CreateMyList/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreateMyList
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];  //çağrıldığında 0 uzunluğunda bir array oluşturduk.
+        }
+
+        public void Add(T item)
+        {
+            T[] tempItems = items;  //geçici arrayimizi oluşturduk.
+
+            items = new T[items.Length + 1]; 
+
+            for (int i = 0; i < tempItems.Length; i++) // geçici arrayda olan bilgileri gerçek arrayimize atadık.
+            {
+                items[i] = tempItems[i];
+            }
+            items[items.Length - 1] = item;
+        }
+        public T[] Items // yazdırma işlemi
+        {
+            get { return items; }
+        }
+
+        public int Length //uzunluğunu bulma işlemi
+        {
+            get { return items.Length; }
+        }
+    }
+}
---
+++ CreateMyList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CreateMyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();   // myListimizi oluşturduk ve içine eleman ekledik.
+            myList.Add("Kiraz");
+            myList.Add("Elma");
+            myList.Add("Armut");
+
+            foreach (var item in myList.Items)  //foreach ile myListimizi döndük.
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
